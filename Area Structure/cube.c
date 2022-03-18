ab7c22cc1a5afa9fe5d5543907219dbec5041b8a
Abhishek <abhishekphd14@gmail.com>
2022-03-18 20:57:34 +0530

    Create cube.c

---
+++ Area Structure/cube.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int cube;
+    printf("Enter a side of Cube: ");
+    scanf ("%d", &cube);
+    printf("answer of cube is : %d", (cube*cube*cube));
+    return 0;
+}
