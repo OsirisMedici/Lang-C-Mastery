239e667bddbdf86273aa225c747d83a1a901e14e
Abhishek <abhishekphd14@gmail.com>
2022-03-19 14:43:02 +0530

    Create add50.c

---
+++ Assignement/add50.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i ;
+    int sum = 0 ;
+
+    for(i=5 ; i <=50; i++){
+        
+
+        sum = sum +i;
+
+    }
+    printf("Total sum is: %d", sum);
+    return 0;
+}
